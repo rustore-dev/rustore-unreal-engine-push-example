10f5506d9d1ef313b5f9e4b8dc209b83de82073e
rpelmegov <rpelmegov@ya.ru>
2023-08-28 15:37:03 +0300

    Update push sdk to 0.6.0

--- Source/RuStorePushApp/RuStorePushApp.Build.cs
+++ Source/RuStorePushApp/RuStorePushApp.Build.cs
@@ -12,19 +12,11 @@ public class RuStorePushApp : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "RuStoreCore", "RuStorePush" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-
+        
 		if (Target.Platform == UnrealTargetPlatform.Android)
 		{
 			PrivateDependencyModuleNames.AddRange(new string[] { "Launch" });
-			AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ModuleDirectory, "RuStorePushStyle_UPL_Android.xml"));
+			AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ModuleDirectory, "RuStorePushApp_UPL_Android.xml"));
 		}
 	}
 }
