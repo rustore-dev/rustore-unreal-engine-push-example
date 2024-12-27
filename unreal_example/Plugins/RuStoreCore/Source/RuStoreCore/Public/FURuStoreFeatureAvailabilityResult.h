cfcd416458d1ce4c9b00996cb328ff932d832088
rpelmegov <rpelmegov@ya.ru>
2024-12-27 11:10:13 +0300

    Added comments

--- unreal_example/Plugins/RuStoreCore/Source/RuStoreCore/Public/FURuStoreFeatureAvailabilityResult.h
+++ unreal_example/Plugins/RuStoreCore/Source/RuStoreCore/Public/FURuStoreFeatureAvailabilityResult.h
@@ -6,19 +6,34 @@
 #include "FURuStoreError.h"
 #include "FURuStoreFeatureAvailabilityResult.generated.h"
 
+/*!
+@brief Проверка доступности функционала.
+*/
 USTRUCT(BlueprintType)
 struct RUSTORECORE_API FURuStoreFeatureAvailabilityResult
 {
 	GENERATED_USTRUCT_BODY()
 
+	/*!
+	@brief Конструктор.
+	*/
 	FURuStoreFeatureAvailabilityResult()
 	{
 		isAvailable = false;
 	}
 
+	/*!
+	@brief
+		Информация о доступности.
+		Если все условия выполняются, возвращается FURuStoreFeatureAvailabilityResult::isAvailable == true.
+		В противном случае возвращается FURuStoreFeatureAvailabilityResult::isAvailable == false.
+	*/
 	UPROPERTY(BlueprintReadWrite)
 	bool isAvailable;
-	
+
+	/*!
+	@brief Информация об ошибке.
+	*/
 	UPROPERTY(BlueprintReadWrite)
 	FURuStoreError cause;
 };
--- unreal_example/Plugins/RuStorePush/Source/RuStorePush/Public/EURuStoreClickActionType.h
+++ unreal_example/Plugins/RuStorePush/Source/RuStorePush/Public/EURuStoreClickActionType.h
@@ -5,10 +5,24 @@
 #include "CoreMinimal.h"
 #include "EURuStoreClickActionType.generated.h"
 
+/*!
+@brief Тип поля clickAction в классе FURuStoreNotification.
+*/
 UENUM(BlueprintType)
 enum class EURuStoreClickActionType : uint8
 {
+    /*!
+    @brief По умолчанию.
+    */
     DEFAULT UMETA(DisplayName = "DEFAULT"),
+    
+    /*!
+    @brief Deeplink.
+    */
     DEEP_LINK UMETA(DisplayName = "DEEP_LINK"),
+    
+    /*!
+    @brief Значение не определено.
+    */
     UNKNOWN UMETA(DisplayName = "UNKNOWN")
 };
